f9d1dd23ab26d13c112b78cda1cd8f8387c0a761
winnowplus <winnowplus@gmail.com>
2025-03-25 13:27:34 +0800

    update function ExportGearStats

--- GearStatsExporter.lua
+++ GearStatsExporter.lua
@@ -23,39 +23,41 @@ local function AddHeader(headers, header)
     end
 end
 
-function ExportGearStats(ids, ret)
-    ret = ret or {};
-    ret.headers = ret.headers or {};
-    ret.gears = ret.gears or {};
+function ExportGearStats(...)
+    local headers, gears = {}, {};
 
-    for _, id in ipairs(ids) do
-        VanillaPlusTooltip:SetOwner(WorldFrame, "ANCHOR_NONE");
-        VanillaPlusTooltip:ClearLines();
-        VanillaPlusTooltip:SetHyperlink("item:" .. id .. ":0:0:0");
-        local gearName = GetItemInfo(id);
+    for _, ids in ipairs(arg) do
+        ids = type(data) == "table" and ids or {ids};
 
-        if(gearName == nil) then
-            DEFAULT_CHAT_FRAME:AddMessage("Gear " .. tostring(id) .. " is not loaded, please try again.");
-        else
-            local gear = {};
-            table.insert(ret.gears, gear);
+        for _, id in ipairs(ids) do
+            VanillaPlusTooltip:SetOwner(WorldFrame, "ANCHOR_NONE");
+            VanillaPlusTooltip:ClearLines();
+            VanillaPlusTooltip:SetHyperlink("item:" .. id .. ":0:0:0");
+            local gearName = GetItemInfo(id);
+
+            if(gearName == nil) then
+                DEFAULT_CHAT_FRAME:AddMessage("Gear " .. tostring(id) .. " is not loaded, please try again.");
+            else
+                local gear = {};
+                table.insert(gears, gear);
             
-            AddHeader(ret.headers, "装备");
-            gear["装备"] = gearName;
+                AddHeader(headers, "装备");
+                gear["装备"] = gearName;
 
-            for line = 1, VanillaPlusTooltip:NumLines() do
-                local widget = _G["VanillaPlusTooltipTextLeft" .. line];
-                local text = widget and widget:GetText();
-
-                if(text ~= nil) then
-                    for pattern, stat in pairs(StatPatterns) do
-                        local _,_, value = string.find(text, pattern);
-
-                        if(value ~= nil) then
-                            AddHeader(ret.headers, stat);
-                            gear[stat] = (gear[stat] or 0) + tonumber(value);
-
-                            break;
+                for line = 1, VanillaPlusTooltip:NumLines() do
+                    local widget = _G["VanillaPlusTooltipTextLeft" .. line];
+                    local text = widget and widget:GetText();
+    
+                    if(text ~= nil) then
+                        for pattern, stat in pairs(StatPatterns) do
+                            local _,_, value = string.find(text, pattern);
+    
+                            if(value ~= nil) then
+                                AddHeader(headers, stat);
+                                gear[stat] = (gear[stat] or 0) + tonumber(value);
+    
+                                break;
+                            end
                         end
                     end
                 end
@@ -63,7 +65,7 @@ function ExportGearStats(ids, ret)
         end
     end
 
-    return ret;
+    return headers, gears;
 end
 
 function ToCsv(headers, data)
